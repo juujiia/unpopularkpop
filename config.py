0c12bd11195265384a1ebba4762676d52e06a552
juujiia <60396379+juujiia@users.noreply.github.com>
2020-01-28 11:00:44 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+id = 'J_5P_arlut5Vcw'
+secret = 'jwpNi1Yvx8gzt-_56uT8E1wvK-0' 	
+username = 'unpopularkpop_mod'
+password = 'unpopularkpop2019'
+user_agent = 'insane/0.2 by Mandjevant' # user agent
+
+sub = 'unpopularkpopopinions' 
+
+initial_comment = "Is this a **Popular** or **Unpopular** opinion? \n Please reply to this comment with either 'popular' or 'unpopular'. If you reply **anywhere else** in the thread, your vote will **not** be counted."
+final_comment = "Voting has concluded. \n This vote was deemed; "
+footer = "\n\n\n\n\n\n^I ^am ^a ^bot ^for ^r/unpopularkpopopinions. ^Please ^send ^me ^a ^message ^if ^you ^have ^any ^feedback ^or ^if ^I ^misbehave."
+removal_type = 'public' # One of ‘public’, ‘private’, ‘private_exposed’. ‘public’ leaves a stickied comment on the post. ‘private’ sends a Modmail message with hidden username. ‘private_exposed’ sends a Modmail message without hidden username
+
+maxtime = 12 # max number of hours the voting is open
